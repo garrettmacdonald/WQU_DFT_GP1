5e46c266bfb335de31672389818c66dee6eb6a32
Garrett MacDonald <garrett.macdonald@gmail.com>
2022-01-09 13:14:40 -0500

    update

--- WorldQuant_Module3_CSA_SkeletonCode/WorldQuant_Module3_CSA_SkeletonCode/Program.cs
+++ WorldQuant_Module3_CSA_SkeletonCode/WorldQuant_Module3_CSA_SkeletonCode/Program.cs
@@ -7,6 +7,13 @@ namespace WorldQuant_Module3_CSA_SkeletonCode
     {
         static Excel.Workbook workbook;
         static Excel.Application app;
+        static Excel.Worksheet worksheet;
+        static string filepath = Environment.CurrentDirectory + @"\property_pricing.xlsx";
+        static string[] headers = { "Size", "Suburb", "City", "Market value" };
+        static int nRows = 1;
+        static float min = 1000000000000000;
+        static float max = 0;
+        static float sum = 0;
 
         static void Main(string[] args)
         {
@@ -14,7 +21,23 @@ namespace WorldQuant_Module3_CSA_SkeletonCode
             app.Visible = true;
             try
             {
-                workbook = app.Workbooks.Open("property_pricing.xlsx", ReadOnly: false);
+                workbook = app.Workbooks.Open(filepath, ReadOnly: false);
+                worksheet = workbook.Worksheets.get_Item(1);
+                
+                // get starting row count, sum, min, max
+                string tempText;
+                
+                nRows++;
+                tempText = worksheet.Cells[nRows, 1].Text as string;
+                while (!string.IsNullOrEmpty(tempText))
+                {
+                    float value = (float)worksheet.Cells[nRows, 4].Value;
+                    sum += value;
+                    min = Math.Min(min, value);
+                    max = Math.Max(max, value);
+                    nRows++;
+                    tempText = worksheet.Cells[nRows, 1].Text as string;
+                }
             }
             catch
             {
@@ -65,7 +88,8 @@ namespace WorldQuant_Module3_CSA_SkeletonCode
                         default:
                             break;
                     }
-                } catch { }
+                }
+                catch { }
             }
 
             // save before exiting
@@ -90,35 +114,61 @@ namespace WorldQuant_Module3_CSA_SkeletonCode
         static void SetUp()
         {
             // TODO: Implement this method
+            workbook = app.Workbooks.Add();
+
+            // set up headers
+            workbook.SaveAs(filepath);
+            worksheet = workbook.Worksheets.get_Item(1);
+            for (int i = 0; i < headers.Length; i++) worksheet.Cells[1, i + 1] = headers[i];
+            nRows++;
+
+            workbook.Save();
         }
 
         static void AddPropertyToWorksheet(float size, string suburb, string city, float value)
         {
-            // TODO: Implement this method
+            // add data to the sheet then adds 1 to the row counter
+            worksheet.Cells[nRows, 1] = size;
+            worksheet.Cells[nRows, 2] = suburb;
+            worksheet.Cells[nRows, 3] = city;
+            worksheet.Cells[nRows, 4] = value;
+            nRows++;
+            
+            sum += value; // for mean calculation
+
+            // update min/max
+            min = Math.Min(min, value);
+            max = Math.Max(max, value);
+            
+            workbook.Save();
         }
 
         static float CalculateMean()
         {
-            // TODO: Implement this method
-            return 0.0f;
+            // mean price is the sum of all prices divided by the number of properties
+            return sum / (nRows - 2);
         }
 
         static float CalculateVariance()
         {
-            // TODO: Implement this method
-            return 0.0f;
+            float mean = CalculateMean(); // need to get mean first to calculate sum of squared errors (sse)
+            // initialize sse, go through each property, find squared error, add to counter
+            double sse = 0;
+            for (int row = 2; row < nRows; row++)
+            {
+                sse += Math.Pow(worksheet.Cells[row, 4].Value - mean, 2);
+            }
+            return (float)sse / (nRows - 3); // divide sse by #obs-1 for sample var
         }
 
         static float CalculateMinimum()
         {
-            // TODO: Implement this method
-            return 0.0f;
+            return min; // already calculated after each property is added
         }
 
         static float CalculateMaximum()
         {
-            // TODO: Implement this method
-            return 0.0f;
+            return max; // already calculated after each property is added
         }
     }
 }
